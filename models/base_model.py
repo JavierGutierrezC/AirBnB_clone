8df082d19efdac64ad213144e0c7da7ced601d31
untaljacko <untaljacko@gmail.com>
2019-11-09 10:36:11 -0500

    just an upload with last changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,25 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'name':
+                    self.name = value
+                elif key == 'my_number':
+                    self.my_number = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return("[{}] ({}) {}".format(
