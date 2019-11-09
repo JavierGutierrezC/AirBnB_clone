b22d132299514d213c4d6ee2df40f36706739a1d
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-09 01:11:34 +0000

    first commit

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Class State  '''
+from models.base--model import BaseModel
+
+
+class State(BaseModel):
+    '''Class State that inherits from BaseModel '''
+
+    name = ""
+
+#def __init__(self.)
+#super.__init__
+
+
---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Class User  '''
+from models.base--model import BaseModel
+
+
+class User(BaseModel):
+    '''Class User that inherits from BaseModel '''
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+#def __init__(self.)
+#super.__init__
+
+
