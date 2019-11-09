8d3cfeb1be5cd2fad03ba1e027a5b5148a6c5619
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-09 19:20:09 +0000

    [Fix]add variable

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-
-from models import Base_Model
+''' '''
+from models.base_model import Base_Model
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import json
 
+
 class FileStorage():
+    ''' '''
+
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        ''' '''
         return FileStorage.__objects
 
     def new(self,obj):
+        ''' '''
         key = obj.__class__.__name__.id + "." obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
+        ''' '''
         dict1 = {}
         for key, value in FileStorage.__objects.items():
             dict1[key] = value.to_dict
         with open("{}.json".format(FileStorage.__file_path), "w") as Newdict:
             json.dump(dict1, Newdict)
+
+    def reload(self):
+        ''' '''
+        
