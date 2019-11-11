96823d92e2b3920b04f3c83c34299326a78e4a35
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-11 01:44:15 +0000

    [Fix]Create/show function

--- console.py
+++ console.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 import cmd
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 ''' Class Console'''
 
+list_class = ["BaseModel", "State", "City", "Amenity", "Place", "Review", "User"]
+
+dict_class = {"State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review, "User": User}
 
 class HBNBCCommand(cmd.Cmd):
     '''Command Cosole'''
-    prompt = "(hbnb)"
+    
+
+    prompt = "(hbnb) "
 
     def emptyline(self):
         '''Goes to the next line'''
@@ -19,6 +32,27 @@ class HBNBCCommand(cmd.Cmd):
     def do_EOF(self, line):
         '''End of file'''
         return True
+
+    def do_create(self, args):
+        if not args:
+            print('** class name missing **')
+        elif args not in list_class:
+            print('** class doesn\'t exist **')
+        else:
+            element = dict_class[args]()
+            print(element.id)
+            element.save()
+        
+    def do_show(self, args):
+        if not args:
+            print('** class name missing **')
+        elif args not in list_class:
+            print('** class doesn\'t exist **')
+
+        else:
+            spl_args = args.split()
+            if len(spl_args) < 2:
+                print('** instance id missing **')
 
 if __name__ == '__main__':
     interprete = HBNBCCommand()
