e348b1afcd953ad357ddef55618c48f10df3d34f
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-13 20:37:26 +0000

    [Add]New tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
+"""
+Unittest for city
+"""
 from models.city import City
 import datetime
 import unittest
-"""Unittests for city"""
 
 
 class CityCase(unittest.TestCase):
+    """Tests for City's attributes and instances """
 
     cityx = City()
 
     def test_hasattr(self):
-        self.assertTrue(hasattr(self.cityx, "name"))
-        # BaseModel Attributes
+        """Tests for attributes"""
+        self.assertTrue(hasattr(self.cityx, "state_id"))
         self.assertTrue(hasattr(self.cityx, "id"))
         self.assertTrue(hasattr(self.cityx, "created_at"))
         self.assertTrue(hasattr(self.cityx, "updated_at"))
 
     def test_types(self):
+        """Test for instances"""
+        self.assertIsInstance(self.cityx..state_id, str)
         self.assertIsInstance(self.cityx.name, str)
-        # BaseModel Attributes
         self.assertIsInstance(self.cityx.id, str)
         self.assertIsInstance(self.cityx.created_at, datetime.datetime)
         self.assertIsInstance(self.cityx.updated_at, datetime.datetime)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+"""
+Unittests for review
+"""
 from models.review import Review
 import datetime
 import unittest
-"""Unittests for review"""
 
 
 class ReviewCase(unittest.TestCase):
-
+    """Tests for methods and instances of Review"""
     reviewx = Review()
 
     def test_hasattr(self):
+        """Test for attributes"""
+        self.assertTrue(hasattr(self.reviewx, "place_id"))
+        self.assertTrue(hasattr(self.reviewx, "user_id"))
+        self.assertTrue(hasattr(self.reviewx, "text"))
         self.assertTrue(hasattr(self.reviewx, "name"))
-        # BaseModel Attributes
         self.assertTrue(hasattr(self.reviewx, "id"))
         self.assertTrue(hasattr(self.reviewx, "created_at"))
         self.assertTrue(hasattr(self.reviewx, "updated_at"))
 
     def test_types(self):
+        """Tests for instances"""
+        self.assertIsInstance(self.reviewx.place_id, str)
+        self.assertIsInstance(self.reviewx.user_id, str)
+        self.assertIsInstance(self.reviewx.text, str)
         self.assertIsInstance(self.reviewx.name, str)
-        # BaseModel Attributes
         self.assertIsInstance(self.reviewx.id, str)
         self.assertIsInstance(self.reviewx.created_at, datetime.datetime)
         self.assertIsInstance(self.reviewx.updated_at, datetime.datetime)
