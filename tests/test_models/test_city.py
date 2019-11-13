928507530fd5a3808b469aa37d737b171892f0ce
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-13 21:41:20 +0000

    [Fix]tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -16,13 +16,15 @@ class CityCase(unittest.TestCase):
         """Tests for attributes"""
         self.assertTrue(hasattr(self.cityx, "state_id"))
         self.assertTrue(hasattr(self.cityx, "name"))
+
+
         self.assertTrue(hasattr(self.cityx, "id"))
         self.assertTrue(hasattr(self.cityx, "created_at"))
         self.assertTrue(hasattr(self.cityx, "updated_at"))
 
     def test_types(self):
         """Test for instances"""
-        self.assertIsInstance(self.cityx..state_id, str)
+        self.assertIsInstance(self.cityx.state_id, str)
         self.assertIsInstance(self.cityx.name, str)
         self.assertIsInstance(self.cityx.id, str)
         self.assertIsInstance(self.cityx.created_at, datetime.datetime)
